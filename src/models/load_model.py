5340715640b9b03bf2db8544e36fc5a1b8b4ded2
Georgia Gunson <ec23045@qmul.ac.uk>
2026-06-15 21:36:05 +0100

    Initial upload of Faster R-CNN model scripts

---
+++ src/models/load_model.py
@@ -0,0 +1,47 @@
+import torchvision
+from models.faster_rcnn_edit import fasterrcnn_resnet18_fpn, fasterrcnn_resnet50_fpn
+
+def load_model(config, device):
+
+    if config['MODEL'] == 'fasterrcnn_resnet18_fpn':
+
+        if config['INFERENCE']:
+            model = fasterrcnn_resnet18_fpn(num_classes=2,
+                                            trainable_backbone_layers=5,
+                                            encoder_weights=config['ENCODER_WEIGHTS'],
+                                            # rpn_fg_iou_thresh=config['RPN_FG_IOU_THRESH'], # train
+                                            # rpn_bg_iou_thresh=config['RPN_BG_IOU_THRESH'], # train
+                                            rpn_pre_nms_top_n_test=config['PRE_NMS_TOP_N_TEST'], # test
+                                            box_nms_thresh=config['NMS_IOU'],
+                                            box_score_thresh=config['CONF_THRESH']
+                                            ).to(device)
+
+        else:
+            model = fasterrcnn_resnet18_fpn(num_classes=2,
+                                            trainable_backbone_layers=5,
+                                            encoder_weights=config['ENCODER_WEIGHTS'],
+                                            ).to(device)
+
+    elif config['MODEL'] == 'fasterrcnn_resnet50_fpn':
+
+        if config['INFERENCE']:
+            model = fasterrcnn_resnet50_fpn(num_classes=2,
+                                            trainable_backbone_layers=5,
+                                            encoder_weights=config['ENCODER_WEIGHTS'],
+                                            # rpn_fg_iou_thresh=config['RPN_FG_IOU_THRESH'], # train
+                                            # rpn_bg_iou_thresh=config['RPN_BG_IOU_THRESH'], # train
+                                            rpn_pre_nms_top_n_test=config['PRE_NMS_TOP_N_TEST'], # test
+                                            box_nms_thresh=config['NMS_IOU'],
+                                            box_score_thresh=config['CONF_THRESH']
+                                            ).to(device)
+
+        else:
+            model = fasterrcnn_resnet50_fpn(num_classes=2,
+                                            trainable_backbone_layers=5,
+                                            encoder_weights=config['ENCODER_WEIGHTS'],
+                                            ).to(device)
+
+    else:
+        print('Model not recognised')
+
+    return model
