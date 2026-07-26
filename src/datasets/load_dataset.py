ec2dd98b1d806b49d512df9a2fa4fd766dc4c325
Georgia Gunson <ec23045@qmul.ac.uk>
2026-07-26 19:21:30 +0100

    Incorporation of temporal dataset into load_dataset and changes to image tensor shape in temporal_sb_dataset_inc_negatives

--- src/datasets/load_dataset.py
+++ src/datasets/load_dataset.py
@@ -1,5 +1,6 @@
 from src.datasets.sb_dataset import SB_Dataset
 from src.datasets.sb_dataset_inc_negatives import SB_Dataset_Inc_Negatives
+from src.datasets.temporal_sb_dataset_inc_negatives import temporal_SB_Dataset_Inc_Negatives
 from torch.utils.data import DataLoader
 from src.datasets.augmentations import get_transform
 from src.detection_utils.utils import collate_fn, collate_fn_inc_negatives
@@ -78,6 +79,44 @@ def load_dataset(config):
                                  num_workers=config['NUM_WORKERS'],
                                  pin_memory=True,
                                  collate_fn=collate_fn_inc_negatives)
+
+        return (train_loader, val_loader, test_loader), (train_dataset, val_dataset, test_dataset)
+
+    elif config['DATASET'] == 'temporal_sb_inc_negatives':
+        train_dataset = temporal_SB_Dataset_Inc_Negatives(data_root=data_root,
+                                    modality=config['MODALITY'],
+                                    subset='train',
+                                    transforms=get_transform(train=config['AUGMENTATION']),
+                                    resolution=config['RESOLUTION'],
+                                    t_frames = config['T_FRAMES'])
+        val_dataset = temporal_SB_Dataset_Inc_Negatives(data_root=data_root,
+                                    modality=config['MODALITY'],
+                                    subset='val',
+                                    transforms=False,
+                                    resolution=config['RESOLUTION'],
+                                    t_frames = config['T_FRAMES'])
+        test_dataset = temporal_SB_Dataset_Inc_Negatives(data_root=data_root,
+                                    modality=config['MODALITY'],
+                                    subset='test',
+                                    transforms=False,
+                                    resolution=config['RESOLUTION'], 
+                                    t_frames = config['T_FRAMES'])
+
+        train_loader = DataLoader(train_dataset,
+                                    batch_size=config['TRAIN_BATCH_SIZE'],
+                                    shuffle=True,
+                                    num_workers=config['NUM_WORKERS'],
+                                    collate_fn=collate_fn_inc_negatives)
+        val_loader = DataLoader(val_dataset,
+                                    batch_size=config['VAL_BATCH_SIZE'],
+                                    num_workers=config['NUM_WORKERS'],
+                                    pin_memory=True,
+                                    collate_fn=collate_fn_inc_negatives)
+        test_loader = DataLoader(test_dataset,
+                                    batch_size=config['VAL_BATCH_SIZE'],
+                                    num_workers=config['NUM_WORKERS'],
+                                    pin_memory=True,
+                                    collate_fn=collate_fn_inc_negatives)
 
         return (train_loader, val_loader, test_loader), (train_dataset, val_dataset, test_dataset)
 
--- src/datasets/temporal_sb_dataset_inc_negatives.py
+++ src/datasets/temporal_sb_dataset_inc_negatives.py
@@ -7,7 +7,7 @@ import glob
 import os
 
 
-class SB_Dataset_Inc_Negatives(Dataset):
+class temporal_SB_Dataset_Inc_Negatives(Dataset):
 
     def __init__(self, data_root, modality, subset, transforms, resolution, t_frames):
         self.transforms = transforms
@@ -144,6 +144,8 @@ class SB_Dataset_Inc_Negatives(Dataset):
 
         image = torch.from_numpy(image).to(torch.float32)
         image = torch.permute(image, (2,0,1)) # (5,224,224)
+        image = image.unsqueeze(1) # (5,1,224,224)
+        print(image.shape)
 
         if len(anno) > 0:
             target['boxes'] = torch.as_tensor(boxes,dtype=torch.float32)
